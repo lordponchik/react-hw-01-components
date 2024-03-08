ba5aa5edbbeb109fde46a235f48b93353f88201d
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-03-08 19:31:44 +0100

    add Profile in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
+import userData from '../data/user.json';
+
+import Section from './Section/Section';
 import Profile from './Profile/Profile';
 
 export const App = () => {
   return (
-    <div>
-      <Profile />
-    </div>
+    <>
+      <Section title="Profile">
+        <Profile
+          username={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          avatar={userData.avatar}
+          stats={userData.stats}
+        ></Profile>
+      </Section>
+    </>
   );
 };
