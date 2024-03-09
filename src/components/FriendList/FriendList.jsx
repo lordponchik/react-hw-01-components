209e3fadcfa9f91468b091515ccfde740573e3ad
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-03-09 16:17:31 +0100

    add component FriendList

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
+import s from './FriendList.module.css';
+
+function FriendList({ friends }) {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(friend => (
+        <FriendListItem key={friend.id} {...friend}></FriendListItem>
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friend: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
