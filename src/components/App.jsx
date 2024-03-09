44fa116fbc32001102826959b2bb605a179b6712
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-03-09 16:18:31 +0100

    add component FriendList in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import userData from '../data/user.json';
 import statisticsData from '../data/data.json';
+import friendsData from '../data/friends.json';
 
 import Section from './Section/Section';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
 
 export const App = () => {
   return (
@@ -19,6 +21,9 @@ export const App = () => {
       </Section>
       <Section title="Statistics">
         <Statistics title="Upload stats" stats={statisticsData}></Statistics>
+      </Section>
+      <Section title="FriendList">
+        <FriendList friends={friendsData}></FriendList>
       </Section>
     </>
   );
