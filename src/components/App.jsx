e4d552074d44682398132eb04460cbf2da9ed880
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-03-09 14:29:17 +0100

    add component Statistics in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import userData from '../data/user.json';
+import statisticsData from '../data/data.json';
 
 import Section from './Section/Section';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 
 export const App = () => {
   return (
@@ -14,6 +16,9 @@ export const App = () => {
           avatar={userData.avatar}
           stats={userData.stats}
         ></Profile>
+      </Section>
+      <Section title="Statistics">
+        <Statistics title="Upload stats" stats={statisticsData}></Statistics>
       </Section>
     </>
   );
